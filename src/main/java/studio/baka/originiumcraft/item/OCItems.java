4601b4949489ef57df3bfbd2503e20c6969bbfd2
许皓钦 <alaricgilbert@qq.com>
2019-06-26 23:23:56 +0800

    chips added

--- src/main/java/studio/baka/originiumcraft/item/OCItems.java
+++ src/main/java/studio/baka/originiumcraft/item/OCItems.java
@@ -11,16 +11,41 @@ public class OCItems {
 
     public static final Item ADVANCED_CERTIFICATE = new ItemBase("advanced_certificate");
     public static final Item ARTIFICIAL_ORIGINIUM = new ItemBase("artificial_originium");
+    public static final Item CASTER_CHIP = new ItemBase("caster_chip");
+    public static final Item CASTER_CHIPSET = new ItemBase("caster_chipset");
+    public static final Item CASTER_DUALCHIPSET = new ItemBase("caster_dualchipset");
     public static final Item COMBAT_RECORD_ADVANCED = new ItemBase("combat_record_advanced");
     public static final Item COMBAT_RECORD_BASIC = new ItemBase("combat_record_basic");
     public static final Item COMBAT_RECORD_MEDIUM = new ItemBase("combat_record_medium");
     public static final Item COMBAT_RECORD_PRIMARY = new ItemBase("combat_record_primary");
     public static final Item CREDIT_POINT = new ItemBase("credit_point");
+    public static final Item DEFENDER_CHIP = new ItemBase("defender_chip");
+    public static final Item DEFENDER_CHIPSET = new ItemBase("defender_chipset");
+    public static final Item DEFENDER_DUALCHIPSET = new ItemBase("defender_dualchipset");
+    public static final Item GUARD_CHIP = new ItemBase("guard_chip");
+    public static final Item GUARD_CHIPSET = new ItemBase("guard_chipset");
+    public static final Item GUARD_DUALCHIPSET = new ItemBase("guard_dualchipset");
     public static final Item LONGMEN_COIN =  new ItemBase("longmen_coin");
+    public static final Item MEDIC_CHIP = new ItemBase("medic_chip");
+    public static final Item MEDIC_CHIPSET = new ItemBase("medic_chipset");
+    public static final Item MEDIC_DUALCHIPSET = new ItemBase("medic_dualchipset");
     public static final Item ORIGINIUM = new ItemOriginium();
     public static final Item PRECLINICAL_ARTS_1 = new ItemBase("preclinical_art_1");
     public static final Item PRECLINICAL_ARTS_2 = new ItemBase("preclinical_art_2");
     public static final Item PRECLINICAL_ARTS_3 = new ItemBase("preclinical_art_3");
     public static final Item PURCHASE_CERTIFICATE = new ItemBase("purchase_certificate");
     public static final Item QUALIFICATION_CERTIFICATE = new ItemBase("qualification_certificate");
+    public static final Item SNIPER_CHIP = new ItemBase("sniper_chip");
+    public static final Item SNIPER_CHIPSET = new ItemBase("sniper_chipset");
+    public static final Item SNIPER_DUALCHIPSET = new ItemBase("sniper_dualchipset");
+    public static final Item SPECIALIST_CHIP = new ItemBase("specialist_chip");
+    public static final Item SPECIALIST_CHIPSET = new ItemBase("specialist_chipset");
+    public static final Item SPECIALIST_DUALCHIPSET = new ItemBase("specialist_dualchipset");
+    public static final Item SUPPORTTER_CHIP = new ItemBase("supporter_chip");
+    public static final Item SUPPORTTER_CHIPSET = new ItemBase("supporter_chipset");
+    public static final Item SUPPORTTER_DUALCHIPSET = new ItemBase("supporter_dualchipset");
+    public static final Item VANGUARD_CHIP = new ItemBase("vanguard_chip");
+    public static final Item VANGUARD_CHIPSET = new ItemBase("vanguard_chipset");
+    public static final Item VANGUARD_DUALCHIPSET = new ItemBase("vanguard_dualchipset");
 }
+
